feb1b7246464d1c9bd226cf4d8e782a99e346edc
Uriel <uriellopezaquino1.0@gmail.com>
2021-10-03 15:18:31 -0500

    Material Button _ Personalization

--- app/src/main/java/com/alain/cursos/mdcomponents/fragments/ButtonFragment.java
+++ app/src/main/java/com/alain/cursos/mdcomponents/fragments/ButtonFragment.java
@@ -8,10 +8,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.alain.cursos.mdcomponents.R;
 import com.alain.cursos.mdcomponents.utils.Component;
 import com.alain.cursos.mdcomponents.utils.Constants;
+
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 
 /**
@@ -22,6 +26,7 @@ public class ButtonFragment extends Fragment {
     public static final String TAG = "Button";
 
     private static Component mInstance;
+    Unbinder mUnbinder;
 
     public ButtonFragment() {
         // Required empty public constructor
@@ -40,6 +45,17 @@ public class ButtonFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_button, container, false);
+    }
+
+    @OnClick(R.id.btnEnable)
+    public void onViewClicked() {
+        Toast.makeText(getActivity(), R.string.status_enabled, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mUnbinder.unbind();
     }
 
 }
