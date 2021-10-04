5ca442065a9592192fa8f5e531a03ce451ed47bf
Uriel <uriellopezaquino1.0@gmail.com>
2021-10-04 13:27:51 -0500

    Text fields Eventos

--- app/src/main/java/com/alain/cursos/mdcomponents/fragments/TextFieldFragment.java
+++ app/src/main/java/com/alain/cursos/mdcomponents/fragments/TextFieldFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,7 +13,10 @@ import android.view.ViewGroup;
 import com.alain.cursos.mdcomponents.R;
 import com.alain.cursos.mdcomponents.utils.Component;
 import com.alain.cursos.mdcomponents.utils.Constants;
+import com.google.android.material.textfield.TextInputEditText;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
 /**
@@ -23,7 +28,15 @@ public class TextFieldFragment extends Fragment {
     public static final String TAG = "Text Fields";
 
     private static Component mInstance;
+
     Unbinder mUnbinder;
+    @BindView(R.id.etPrice)
+    TextInputEditText etPrice;
+    @BindView(R.id.etCapitalLetter)
+    TextInputEditText etCapitalLetter;
+    @BindView(R.id.tilCapitalLetter)
+    TextInputEditText tilCapitalLetter;
+
 
     public static Component getmInstance(){
         mInstance = new Component();
@@ -43,6 +56,35 @@ public class TextFieldFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_text_field, container, false);
+        View view = inflater.inflate(R.layout.fragment_text_field, container, false);
+        mUnbinder = ButterKnife.bind( this, view);
+
+        etPrice.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (editable.toString().isEmpty() && Integer.valueOf(editable.toString()) < 5) {
+                    etPrice.setError(getString(R.string.error_price_min));
+                }
+            }
+        });
+
+        return view;
     }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mUnbinder.unbind();
+    }
+
 }
